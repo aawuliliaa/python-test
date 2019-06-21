fa877218705839d0d1bd76e654b13accdcd827e8
vita <vita@qq.com>
2019-06-21 15:35:45 +0800

    我的web框架

--- excercise/9.Django/1.myWebStruct/main.py
+++ excercise/9.Django/1.myWebStruct/main.py
@@ -10,14 +10,17 @@ def application(environ, start_response):
     path = environ.get("PATH_INFO")
     start_response("200 OK", [])
     func = None
+    # url_patterns是一个列表，列表中的每项是一个数组
     for item in url_patterns:
         if path == item[0]:
             func = item[1]
             break
 
     if func:
+        # 这种return []是wsgiref模块规定的模式
         return [func(environ)]
+if __name__ == '__main__':
 
-
-httpd = make_server("127.0.0.1", 8800, application)
-httpd.serve_forever()
+    # 启动socket服务，等待连接
+    httpd = make_server("127.0.0.1", 8800, application)
+    httpd.serve_forever()
--- excercise/9.Django/1.myWebStruct/views.py
+++ excercise/9.Django/1.myWebStruct/views.py
@@ -30,11 +30,12 @@ def auth(environ):
     request_body = environ['wsgi.input'].read(request_body_size)
     data = parse_qs(request_body)
     # 我这个是windows系统，所以这里要以gbk解码，如果是linux或mac,要用utf8解码
+    # 获取页面上的用户名和密码
     user = data.get(b"user")[0].decode("gbk")
     pwd = data.get(b"pwd")[0].decode("gbk")
 
     cur = get_data(user, pwd)
-    # 如果用户名，密码正确
+    # 如果用户名，密码正确，就展示index页面
     if cur.fetchone():
         return index(environ)
     else:
