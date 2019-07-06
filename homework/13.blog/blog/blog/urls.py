7c176e38653e734ea0545525c44f0d30610b18a3
vita <vita@qq.com>
2019-07-06 21:27:14 +0800

    博客

--- homework/13.blog/blog/blog/urls.py
+++ homework/13.blog/blog/blog/urls.py
@@ -23,9 +23,9 @@ urlpatterns = [
     # 这里是滑动验证码处使用的ajax
     re_path(r'^pc-geetest/register', views.pcgetcaptcha, name='pcgetcaptcha'),
     re_path(r'^pc-geetest/ajax_validate', views.pcajax_validate, name='pcajax_validate'),
-    path('login/', views.login),
-    path('logout/', views.logout),
-    path('register/', views.register),
+    path('login/', views.login, name="login"),
+    path('logout/', views.logout, name="logout"),
+    path('register/', views.register, name="register"),
     # 获取评论树
     path('get_comment_tree/', views.get_comment_tree),
     # 提交评论
@@ -33,7 +33,7 @@ urlpatterns = [
     # 点赞按钮
     path('up_down/', views.up_down),
     re_path('^$', views.index),
-    path('back_manage/', views.back_manage),
+    path('back_manage/', views.back_manage, name="back_manage"),
     path('add_article/', views.add_article),
     # kindeditor编辑器中的图片上传
     path('upload/', views.upload),
@@ -43,7 +43,7 @@ urlpatterns = [
 
 
     # 这里如果不在index/后加个$结尾，访问index/的时候，，页面的中图片就不显示
-    re_path('index/$', views.index),
+    re_path('index/$', views.index, name="index"),
     re_path('^(?P<username>[a-zA-Z]+)/articles/(?P<article_id>[0-9]+)$', views.article_detail),
     # media配置:只有配置了这里，Index页面中才能显示出头像
     # 这里要注意，当url多了的时候，就会出现匹配的顺序问题，
