03cd621b147f85031c24e4f43f9d06f8230b5742
vita <vita@qq.com>
2019-08-09 12:10:50 +0800

    CMDB

--- CMDB/monitor/urls.py
+++ CMDB/monitor/urls.py
@@ -11,5 +11,9 @@ urlpatterns = [
     path('add_template/', login_required(views.AddTemplate.as_view()), name="add_template"),
     path('del_template/<int:pk>/', login_required(views.DelTemplate.as_view()), name="del_template"),
     path('edit_template/<int:pk>/', login_required(views.EditTemplate.as_view()), name="edit_template"),
+    path('monitor_item/', login_required(views.MonitorItemView.as_view()), name="monitor_item"),
+    path('add_monitor_item/', login_required(views.AddMonitorItem.as_view()), name="add_monitor_item"),
+    path('del_monitor_item/<int:pk>/', login_required(views.DelMonitorItem.as_view()), name="del_monitor_item"),
+    path('edit_monitor_item/<int:pk>/', login_required(views.EditMonitorItem.as_view()), name="edit_monitor_item"),
 
 ]
--- CMDB/monitor/views.py
+++ CMDB/monitor/views.py
@@ -43,7 +43,7 @@ class AddTemplate(View):
     def get(self, request):
         left_label_dic = get_label(request)
         form = TemplateForm()
-        return render(request, 'asset/add_edit_env.html', locals())
+        return render(request, 'monitor/add_edit_template.html', locals())
 
     def post(self, request):
         left_label_dic = get_label(request)
@@ -87,8 +87,7 @@ class EditTemplate(View):
         # 这样既能利用modelform的优势，也能灵活操作。
         # 由于不可为空和长度限制已经在前端限制好了，后端就不需要做检查了
         name = request.POST.get("name")
-        abs_name = request.POST.get("abs_name")
-        template_set = Template.objects.filter(name=name, abs_name=abs_name)
+        template_set = Template.objects.filter(name=name)
         template_set.update(note=request.POST.get("note"))
         template_obj = template_set.first()
 
@@ -100,3 +99,88 @@ class EditTemplate(View):
         template_obj.monitor_item.set(monitor_item_list)
 
         return redirect(reverse("monitor:template"))
+
+
+class MonitorItemView(View):
+    """
+    模板信息
+    """
+
+    def get(self, request, *args, **kwargs):
+        # # 批量创建测试数据
+        # list = []
+        # for i in range(101,201):
+        #     item = Environment(name="env_%s" % i, abs_name="env_%s" % i,
+        #     note="env_%s" % i)
+        #     list.append(item)
+        #
+        # Environment.objects.bulk_create(list)
+        left_label_dic = get_label(request)
+        # print(request.path)# /privilege/
+        role_obj = Role.objects.filter(url=request.path).first()
+
+        data_obj_set = MonitorItem.objects.all()
+        # 这里和面的*("name", "abs_name")是前端的搜索功能，这里是搜索的字段
+        data_page_info = return_show_data(request, data_obj_set, *("name"))
+        return render(request, 'monitor/monitor_item.html', locals())
+
+
+class AddMonitorItem(View):
+    """
+    添加模板信息
+    """
+    def get(self, request):
+        left_label_dic = get_label(request)
+        form = MonitorItemForm()
+        return render(request, 'monitor/add_edit_monitor_item.html', locals())
+
+    def post(self, request):
+        left_label_dic = get_label(request)
+        form = MonitorItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse("monitor:monitor_item"))
+        return render(request, 'monitor/add_edit_monitor_item.html', locals())
+
+
+class DelMonitorItem(View):
+    """
+    删除模板信息
+    """
+    def get(self, request, **kwargs):
+        # print("================2",kwargs)  #{'pk': 205}
+        MonitorItem.objects.filter(id=kwargs.get("pk")).delete()
+        return redirect(reverse("monitor:monitor_item"))
+
+
+class EditMonitorItem(View):
+    """
+    编辑功能就使用了modelform自带的逻辑，如果想自定义逻辑，可能还是使用form把
+    """
+
+    edit_monitor_item = None
+
+    def get(self, request, **kwargs):
+        left_label_dic = get_label(request)
+        self.edit_monitor_item = MonitorItem.objects.filter(pk=kwargs.get("pk")).first()
+        # 创建form对象，传入参数就可以显示之前客户写入的值,ModelForm具备instance参数
+        form = MonitorItemForm(instance=self.edit_monitor_item)  # 接收实例对象
+        return render(request, "monitor/add_edit_monitor_item.html", locals())
+
+    def post(self, request, **kwargs):
+        # 这里需要注意，由于设置了联合主键，如果修改的时候修改成与现有的内容重复了，会放弃修改，
+        # 现象是提交的时候不会报错，只是你会发现没有修改成功
+        # 我这里设置了联合主键name和abs_name,发现如果这两个值不修改，修改其他内容是修改不成功的
+        # 因为会先去数据库中查询，如果存在，就报已经存在
+        # 但是我不希望这样，所以就自己在前端设置name和abs_name不可编辑，其他内容手动插入，就没有使用form了
+        # 这样既能利用modelform的优势，也能灵活操作。
+        # 由于不可为空和长度限制已经在前端限制好了，后端就不需要做检查了
+        name = request.POST.get("name")
+        monitor_item_set = MonitorItem.objects.filter(name=name)
+        monitor_item_set.update(note=request.POST.get("note"),
+                                monitor_script=request.POST.get("monitor_script"),
+                                warn_expression=request.POST.get("warn_expression"),
+                                time_interval=request.POST.get("time_interval"),
+                                warn_type=request.POST.get("warn_type"))
+
+        return redirect(reverse("monitor:monitor_item"))
